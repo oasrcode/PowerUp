7a448beb0b6d81f8a01abf6d0e26d7e28095761a
oasrcode <oasrcode@gmail.com>
2022-11-30 16:55:56 +0000

    profile form

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -1,38 +1,67 @@
 export function Profile() {
+  const enableInput = false; //false to activate
+  const name = "sadasdad";
   return (
-    <div className="flex  overflow-auto flex-col w-full max-w-full ">
-      <div className="flex flex-col md:flex-row w-full h-full ">
-        <div className="flex flex-col w-[50%] space-y-14">
-          <h3 className="font-semibold text-black text-2xl text-center pt-14">
-            Datos Personales
-          </h3>
-          <form>
-            <div className="flex flex-row items-center justify-evenly space-x-8 py-10">
-              <label className="pl-4">Nombre</label>
-              <input
-                className="w-96 py-3 rounded-md "
-                type={"text"}
-               
-              ></input>
-            </div>
-            <div className="flex flex-row items-center justify-evenly space-x-8 py-10">
-              <label className="pl-4">Apellidos</label>
-              <input
-                className="w-96 py-3 rounded-md "
-                type={"text"}
-               
-              ></input>
-            </div>
-            <div className="flex flex-row items-center pl-10 space-x-10 py-10">
-              <label className="pl-4">Edad</label>
-              <input
-                className="w-26 py-3 rounded-md "
-                type={"number"}
-                placeholder="Edad"
-              ></input>
-            </div>
-            
-          </form>
+    <div className="flex flex-col w-full max-w-full h-full">
+      <h2 className="font-semibold text-2xl  text-center pt-10">Mi perfil</h2>
+      <div className="flex flex-col  w-full h-auto lg:w-2/5 lg:mx-auto my-auto pl-4 pr-10">
+        <div className="flex flex-col space-y-3">
+          <label className="font-semibold text-black text-lg">Nombre</label>
+          <input
+            className="p-3 rounded-md w-full "
+            type={"text"}
+            placeholder="nombre"
+          ></input>
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <label className="font-semibold text-black text-lg">Apellido</label>
+          <input
+            className="p-3 rounded-md w-full"
+            type={"text"}
+            placeholder="Apellidos"
+          ></input>
+        </div>
+
+        <div className="flex flex-col space-y-3 w-44">
+          <label className="font-semibold text-black text-lg">
+            Fecha de nacimiento
+          </label>
+          <input
+            className="p-3 rounded-md w-full"
+            type={"date"}
+            placeholder="nombre"
+          ></input>
+        </div>
+
+        <div className="flex flex-col space-y-3 w-44">
+          <label className="font-semibold text-black text-lg">Peso</label>
+          <input
+            className="p-3 rounded-md w-full"
+            type={"number"}
+            placeholder="peso en kg"
+          ></input>
+        </div>
+
+        <div className="flex flex-col space-y-3 w-44">
+          <label className="font-semibold text-black text-lg">Altura</label>
+          <input
+            className="p-3 rounded-md w-full"
+            type={"number"}
+            placeholder="altura en cm"
+          ></input>
+        </div>
+
+        <div className="flex flex-row justify-end pr-5 gap-4 pt-10">
+          <button className="px-8 py-2 bg-slate-200 text-black rounded-md hover:bg-slate-400">
+            Altura
+          </button>
+          <button
+            className="px-8 py-2 bg-slate-200 text-black rounded-md disabled:bg-slate-800 disabled:text-gray-200"
+            disabled={true}
+          >
+            Guardar
+          </button>
         </div>
       </div>
     </div>
